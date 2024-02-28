ac6e72aedea41bf296f48acf11d4d0eb1a224d16
krmacdonald <krmacdonald@quinnipiac.edu>
2024-02-28 11:05:28 -0500

    Added physics material + added to snowmobile script
    Snowmobile script now moves based on speed and with its rigidbody, might go with a different form of movement. Added physics materials to simulate friction with the snow, can be useful for icy patches.

--- Assets/Scripts/Snowmobile Moving Box/TestSnowmobileMovement.cs
+++ Assets/Scripts/Snowmobile Moving Box/TestSnowmobileMovement.cs
@@ -6,18 +6,22 @@ public class TestSnowmobileMovement : MonoBehaviour
 {
     /*
      * @author kylem
-     * @date 2/21/2024
+     * @date 2/28/2024
      * Description: This script acts as the main controller for the snowmobile. Uses W as the gas, S for the brake, and A/D to control turn direction
-     * Prerequisites: A 3D gameObject with a rigidbody
+     * Prerequisites: A 3D gameObject with a rigidbody, "driving" on a material with the snowy physics material applied
      */
 
     /*
      * Additional notes
      * Snowmobile top speed is ~95 miles per hour
      * No reverse on a typical snowmobile
+     * Movement is controlled by a rb with pushing it forward, be conscious of gravity
      * 
      * Goals for next ver: 
      */
+
+    //This gameobject's rigidbody
+    private Rigidbody snowmobileRB;
 
     //Acceleration vars
     [SerializeField]
@@ -32,15 +36,22 @@ public class TestSnowmobileMovement : MonoBehaviour
     //Speed vars
     public float speed; //public to send this value to the speed display
     private float speedCap;
+    [SerializeField]
+    private float speedModifier; // 0-1, divides the speed to match the map
 
     //Brake effectiveness
+    [SerializeField]
     private float brakeEffectiveness;
+
+    //Force being added to the rb
+    private Vector3 movement;
 
 
 
     void Start()
     {
         speedCap = 95f; //speed will be 1:1 in comparison to MPH
+        snowmobileRB = gameObject.GetComponent<Rigidbody>(); //Gets the rb
     }
 
     void Update()
@@ -51,22 +62,32 @@ public class TestSnowmobileMovement : MonoBehaviour
         {
             if (acceleration <= accelCap)
             {
-                acceleration += accelIncrease;
+                acceleration += accelIncrease * Time.deltaTime;
             }
             else
             {
                 acceleration = accelCap;
             }
+        }else if(Input.GetAxis("Drive") < 0f) //Handles the logic for breaking if the player hits the "backwards" button
+        {
+            if(acceleration > 0f)
+            {
+                acceleration = 0f;
+            }
+            if(speed > 0f)
+            {
+                speed -= brakeEffectiveness * Time.deltaTime;
+            }
         }
 
-        //Slows the snowmobile acceleration down when the player is not accelerating
-        if (speed > 0f && acceleration != 0f)
+        //Slows the snowmobile speed down when the player is not accelerating
+        if (speed > 0f)
         {
-            acceleration -= 1f;
+            speed -= 1f * Time.deltaTime;
         }
-        else if (acceleration < 0f)
+        else if (speed < 0f)
         {
-            acceleration = 0f;
+            speed = 0f;
         }
 
         //VERY unlikely to be used unless user is being very unsafe. might remove
@@ -74,5 +95,11 @@ public class TestSnowmobileMovement : MonoBehaviour
         {
             speed = speedCap;
         }
+
+        speed += acceleration;
+
+        //Moves the snowmobile forward based on the current speed
+        movement = (transform.forward * (speed)) / speedModifier;
+        snowmobileRB.AddForce(movement);
     }
 }
