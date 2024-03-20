504247ff2ba2fbe385f0e3f6e24fbbbdc87e9589
krmacdonald <krmacdonald@quinnipiac.edu>
2024-03-20 11:09:40 -0400

    Crash Starter Scene

---
+++ Assets/Scripts/Crash Detection/SnowmobileCrash.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author Kyle Macdonald
+ * @date 3/20/24
+ * prerequisite: Snowmobile prefab, colliders in level with tag "crashable"
+ */
+
+public class SnowmobileCrash : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
