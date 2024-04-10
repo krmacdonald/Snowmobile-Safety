8153fa8d7de9b1823293e795c329e3229a5084c3
tjjoslin <124922955+tjjoslin@users.noreply.github.com>
2024-04-09 23:02:47 -0400

    AI with Navmesh

---
+++ Assets/Scripts/NPC Ai/LoopingFollow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LoopingFollow : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        SetDestination();
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.1f && !agent.pathPending)
+        {
+            SetDestination();
+        }
+    }
+
+    void SetDestination()
+    {
+        if (waypoints.Length == 0)
+            return;
+
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        transform.LookAt(waypoints[currentWaypointIndex].position);
+    }
+}
