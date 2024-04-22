907a0618ef22aa3e60faad72a081b8769f0bf8ce
tjjoslin <124922955+tjjoslin@users.noreply.github.com>
2024-04-22 08:41:39 -0400

    updated payer navmesh

--- Assets/Scripts/NPC Ai/ToClose.cs
+++ Assets/Scripts/NPC Ai/ToClose.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class ToClose : MonoBehaviour
 {
     [SerializeField] GameObject Alert;
+    
 
 
+
+    private void Start()
+    {
+        Alert.SetActive(false);
+
+    }
     private void OnTriggerEnter(Collider other)
     {
+       
+
         if (other.CompareTag("Player"))
         {
             Alert.SetActive(true);
         }
-       
+
     }
 
     private void OnTriggerExit(Collider other)
@@ -29,7 +39,7 @@ public class ToClose : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            Alert.SetActive(false);
+            //Alert.SetActive(false);
         }
     }
 }
