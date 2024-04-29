72792371624084de995871ff2e96d450d96b4551
krmacdonald <krmacdonald@quinnipiac.edu>
2024-04-29 10:37:20 -0400

    Score Text Manip

--- Assets/Scripts/CourseUI/ScoreFill.cs
+++ Assets/Scripts/CourseUI/ScoreFill.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreFill : MonoBehaviour
 {
     public Image ScoreImage;
+    public TMP_Text scoreText;
+    private float score = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         //Score starts full
-        ScoreImage.fillAmount = 1f;
+        ScoreImage.fillAmount = score/100;
+        scoreText.text = string.Concat("Score: ", score.ToString(), "+");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void decreaseScore(float amount)
     {
-        //Score goes down by 10%
-        //ScoreImage.fillAmount -= 0.1;
+        score -= amount;
+        ScoreImage.fillAmount = score / 100;
+        scoreText.text = string.Concat("Score: ", score.ToString(), "+");
+    }
+
+    public void increaseScore(float amount)
+    {
+        score += amount;
+        ScoreImage.fillAmount = score / 100;
+        scoreText.text = string.Concat("Score: ", score.ToString(), "+");
     }
 }
